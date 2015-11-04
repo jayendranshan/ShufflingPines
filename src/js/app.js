43bb91c16553ada6f3e75b634da68d66c563e0e6
Jayendran Shanmugam <Jayendran@Jayendrans-MacBook-Pro.local>
2015-11-04 01:29:36 -0500

    deleted node and bower modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,28 +1,46 @@
 var app = angular.module('shuffling', []);
 
-app.controller('ShufflingController',['Guest','GuestSvc',function(Guest,GuestSvc){
+app.controller('ShufflingController',['Guest','GuestSvc','$scope',function(Guest,GuestSvc,$scope){
  	var vm = this;
 
  	vm.guest=Guest;
+ 	$scope.submitted = false;
 
  	vm.Add = function(){ 
- 		GuestSvc.Add(vm.guest);
+ 		 if ($scope.GuestForm.$valid) {
+ 		 	GuestSvc.Add(vm.guest);
+    		}	
  	};
+
+ 	vm.DropOff = function(){ 
+ 		vm.guest.address='';
+ 	};
+
 }]);
 
 
-app.controller('GuestListController', ['Guest','GuestSvc',function(Guest,GuestSvc){
+app.controller('GuestListController', ['Guest','GuestSvc','$scope',function(Guest,GuestSvc,$scope){
 	var glc = this;
 	glc.guestsList=[];
 	glc.guestsList = GuestSvc.list();
 	glc.newguest=Guest;
 
+	glc.readonly = true;
+	glc.newStatus = '';
 	glc.remove = function(index){
     	GuestSvc.remove(index);
   	};
 
   	glc.Update = function(index){
-    	GuestSvc.Update(index,glc.newguest);
+	    	GuestSvc.Update(index,glc.newguest);
+	    	glc.guestsList = GuestSvc.list();
+	    	glc.readonly = true;
+  	};
+
+  	glc.showEdit = function(index){
+    	glc.readonly = !glc.readonly;
+    	glc.newStatus = GuestSvc.statusChange(index);
+    	
   	};
 
 }]);
@@ -33,13 +51,14 @@ app.value('Guest', {name: "Jay", pickupdrop: "pickup"});
 app.service('GuestSvc', function(){
 
 var guestsList = [];
+var newStatus = '';
 
   this.Add = function(Guest){
 
   		guestsList.push({
  			guestname : Guest.name,
  			date : Guest.date,
- 			status: Guest.pickupdrop,
+ 			status: Guest.status,
  			address: Guest.address
  		});
 
@@ -64,7 +83,7 @@ var guestsList = [];
     };
 
    this.get = function (index) {
-        for (i in guestsList) {
+        for (var i in guestsList) {
             if (guestsList[i].index === index) {
                 return guestsList[i];
             }
@@ -84,5 +103,31 @@ var guestsList = [];
         localStorage.setItem('GuestList', guestsList);
         console.log(guestsList);
   };
+
+  this.statusChange = function (index) {
+        //var guestDetails = this.get(index);
+        console.log(guestsList);
+        console.log(index);
+        for (var i in guestsList) {
+        	console.log(index);
+            if (guestsList[i].index === index) {
+            	console.log('inside');
+                console.log(guestsList[i]);
+            }
+        }
+        //if(guestDetails.status == 'pickup')
+		    	//{
+		    		//newStatus = 'arrived';
+		    	//}
+		    	//else if (guestDetails.status == 'dropoff')
+		    	//{
+		    		//newStatus = 'arrived';
+		    	//}
+		    	//else if (guestDetails.status == 'arrived')
+		    	//{
+		    		//newStatus = 'pickup';
+		    	//}
+        return newStatus;
+    };
 
 });
